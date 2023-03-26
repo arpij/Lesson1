2817865fcab7c5713c550836bb6123afcf36e970
arpi-jaghatspanyan <arpine.jaghatspanyan@picsart.com>
2023-03-26 15:54:07 +0400

    change commit

---
+++ src/Lesson1.java
@@ -0,0 +1,33 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Lesson1");
+
+        byte x = 127;
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
+
+        short y = 4542;
+        System.out.println(y);
+
+        int a = 45678;
+        System.out.println(a);
+
+        long b = 4465L;
+        System.out.println(b);
+
+        char tt = '\u003F';
+        System.out.println(tt);
+
+        String s = "Hello from Java lesson";
+        System.out.println(s);
+
+        String firstName = "Arpine";
+        String lastName = "Jaghatspanyan";
+        System.out.println(firstName +" " + lastName);
+
+        int k = 94;
+        System.out.println("k = " + k);
+
+    }
+}
